b9bcf715d1ac2158c074ead889f47f6ffb3ec678
Ankit Sangwan <ankitsangwan0705@gmail.com>
2025-07-11 17:12:37 +0530

    final axios

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,16 @@ const port = process.env.web_Port || 5000
 // };
 
 //Middlewares
+app.use((req, res, next) => {
+  // Allow requests from your specific origin
+  res.setHeader('Access-Control-Allow-Origin', 'https://recipe-book-swart-pi.vercel.app');
+  // Or allow from any origin (less secure)
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(cors({origin:'*',credential:true}));
 app.use(express.json()); 
 
